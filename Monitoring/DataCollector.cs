b7f40b6c8c8679de84251f8debec425a5b15e482
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-04-19 22:09:38 +0430

    Service

---
+++ Monitoring/DataCollector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Monitoring
+{
+    public sealed class DataCollector
+    {
+        private static DataCollector _collector=null;
+        private static object padLock=new object();
+
+        public static DataCollector Collector
+        {
+            get {
+                lock (padLock)
+                {
+                    if (_collector == null)
+                    {
+                        _collector=new DataCollector();
+                    }
+
+                    return _collector;
+                }
+            }
+        }
+
+        public DataCollector()
+        {
+            
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
