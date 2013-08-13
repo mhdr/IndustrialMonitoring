1687eaf07ad82b71050b58ac694d5a7c3deeae48
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-08-13 16:03:16 +0330

    va belakhare movafagh shodam wsDualhttpBinding ro config konam

---
+++ MonitoringServiceLibrary/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServiceLibrary
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
---
+++ MonitoringServiceLibrary/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringServiceLibrary
+{
+    class UserService:IUserService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
