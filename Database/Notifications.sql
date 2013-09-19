5256842417592e066e8f10c2b2d6d90c9d73c96d
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-09-19 15:27:42 +0330

    database ghesmate notification ro taghir dadam ama hanooz be model emalesh nakardam

--- Database/Notifications.sql
+++ Database/Notifications.sql
@@ -5,8 +5,31 @@
 	[NotificationTitle] NVARCHAR(MAX) NOT NULL, 
     [NotificationDescription] NVARCHAR(MAX) NULL, 
 	[HighLimit] FLOAT NULL, 
-    [LowLimit] FLOAT NULL, 
+    [LowLimit] FLOAT NULL,
+	[NotificationPriority] INT NOT NULL DEFAULT 0,  
     [AutoAcknowledge] BIT NOT NULL DEFAULT 1, 
+    [IsSilent] BIT NOT NULL DEFAULT 0, 
+    [AutoUnmute] BIT NOT NULL DEFAULT 1, 
+    [UnmuteDate] DATETIME2 NULL, 
     CONSTRAINT [FK_Notifications_Items] FOREIGN KEY (ItemId) REFERENCES Items(ItemId), 
     
 )
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'0 : not auto',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Notifications',
+    @level2type = N'COLUMN',
+    @level2name = N'AutoUnmute'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'1 , 2 , 3 , 4 , 5',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Notifications',
+    @level2type = N'COLUMN',
+    @level2name = N'NotificationPriority'
---
+++ Database/NotificationsSilenceHistory.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[NotificationsSilenceHistory]
+(
+	[SilenceId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [NotificationId] INT NOT NULL, 
+    [UserId] INT NOT NULL, 
+	[SilenceStatus] INT NOT NULL, 
+    [ActionDate] DATETIME2 NOT NULL, 
+    CONSTRAINT [FK_NotificationsSilenceHistory_Notifications] FOREIGN KEY (NotificationId) REFERENCES Notifications(NotificationId), 
+    CONSTRAINT [FK_NotificationsSilenceHistory_Users] FOREIGN KEY (UserId) REFERENCES Users(UserId),
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'0 , 1',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'NotificationsSilenceHistory',
+    @level2type = N'COLUMN',
+    @level2name = N'SilenceStatus'
