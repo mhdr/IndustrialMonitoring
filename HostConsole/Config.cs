0b44fc37c92e37d93732db17e733e22afe560b88
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-10-30 16:16:30 +0330

    fix again

---
+++ HostConsole/Config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HostConsole
+{
+    public class Config
+    {
+        public string SystemManagerPath;
+
+        public static Config LoadConfig()
+        {
+            try
+            {
+                string path = "Config.json";
+                StreamReader streamReader = new StreamReader(path);
+                string jsonStr = streamReader.ReadToEnd();
+                Config config = JsonConvert.DeserializeObject<Config>(jsonStr);
+                streamReader.Close();
+                return config;
+            }
+            catch (Exception)
+            {
+                // TODO behtare ke to log ham zakhire beshe
+                Config config = new Config();
+                config.Initialize();
+                return config;
+            }
+        }
+
+        public void Initialize()
+        {
+            this.SystemManagerPath = "";
+        }
+    }
+}
